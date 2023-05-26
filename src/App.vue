2aff30fd206113755dc95e96e9a66d1e0ef0b27c
jiashi <jiashi@qq.com>
2023-05-26 10:21:08 +0800

    测试格式化前

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,20 @@
 <script setup lang="ts">
+
+import { reactive } from "vue";
+
+var obj = reactive({
+  name :"hello"
+});
+
 </script>
 
 <template>
-  <div>
-   hello
-  </div>
+  <div class="box">{{obj.name}}</div>
 </template>
 
 <style scoped>
+.box{
+  width: 20px;
+  height: 500px;background-color: aqua;
+}
 </style>
